55b3a07d15e7811fb9d803a8c1da0baea983eaf1
affjljoo3581 <affjljoo3581@gmail.com>
2020-11-02 21:25:52 +0900

    .

--- sparse/native/sparse_ops.cc
+++ sparse/native/sparse_ops.cc
@@ -3,7 +3,7 @@
 #include "sparse_ops.h"
 
 
-PYBIND11_MODULE(TORCH_EXTENSION, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("sparse_matmul",
           &sparse_matmul_single,
           "Sparse matrix multiplication for single precision.");
