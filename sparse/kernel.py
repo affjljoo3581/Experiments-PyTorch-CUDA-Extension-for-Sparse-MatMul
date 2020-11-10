28633660de40ba3a778812ccc9c4e445de069ba8
affjljoo3581 <affjljoo3581@gmail.com>
2020-11-10 21:25:07 +0900

    .

--- sparse/kernel.py
+++ sparse/kernel.py
@@ -9,7 +9,8 @@ _source_files = [os.path.join(_current_dir, file)
                  if file.endswith(('.cu', '.cc'))]
 
 sparse_ops = torch.utils.cpp_extension.load(
-    'sparse_ops', _source_files,
+    'sparse_ops',
+    _source_files,
     extra_cuda_cflags=['-U__CUDA_NO_HALF_OPERATORS__',
                        '-U__CUDA_NO_HALF_CONVERSIONS__',
                        '-U__CUDA_NO_HALF2_OPERATORS__'])
