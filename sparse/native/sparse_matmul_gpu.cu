af7cad0211148c00e2b236b8f0d83021b393b535
affjljoo3581 <affjljoo3581@gmail.com>
2020-11-14 08:26:04 +0900

    .

--- sparse/native/sparse_matmul_gpu.cu
+++ sparse/native/sparse_matmul_gpu.cu
@@ -9,7 +9,7 @@
 #include "sparse_kernels.h"
 #include "sparse_layout.cuh"
 
-#define USE_32x32_TILING
+#define USE_32x32_TILING_NO_BUFFERING
 
 /**
  * Compute sparse matrix multiplication with SDD mode.
